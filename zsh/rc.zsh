8d086f26967ac52d2b4da94fa20888bcd2a632bd
David Asabina <vid@bina.me>
2018-02-02 13:07:07 +0100

    lit programming makes sense for configs :wink:

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,4 +1,8 @@
-source ~/.defaults
+source ~/.defaults # loads aliases and exports from ~/.defaults
 
-setopt histignorespace
+setopt histignorespace # keeps lines preceded with SPACE out of history
+# From a security perspective this is a wise choice, since you do not want any
+# users on your terminal from being able to potentially discover very sensitive
+# information by checking out the previous commands.
+
 plugins=(pass, history-substring-search)
