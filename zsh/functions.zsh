bb6dedc73ab9da5b3e91d370178ec68729b39e63
David Asabina <vid@bina.me>
2021-01-17 15:21:03 +0100

    feat: Provide convenience cmd wrappers

---
+++ zsh/functions.zsh
@@ -0,0 +1,26 @@
+man () {
+  LESS_TERMCAP_mb=$'\e'"[1;31m" \
+  LESS_TERMCAP_md=$'\e'"[1;31m" \
+  LESS_TERMCAP_me=$'\e'"[0m" \
+  LESS_TERMCAP_se=$'\e'"[0m" \
+  LESS_TERMCAP_so=$'\e'"[1;44;33m" \
+  LESS_TERMCAP_ue=$'\e'"[0m" \
+  LESS_TERMCAP_us=$'\e'"[1;32m" \
+  command man "$@"
+}
+
+grep () {
+  command grep --color=always "$@"
+}
+
+egrep () {
+  command egrep --color=always "$@"
+}
+
+tree () {
+  command tree -C "$@"
+}
+
+dict () {
+  command dict "$@" | colorit
+}
