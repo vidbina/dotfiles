d959b6cdb51e9b5939b93b8ecec04bb24c9ecb7f
David Asabina <vid@bina.me>
2021-04-18 00:21:53 +0200

    chore: Show Emacs linenum + column width indicator

--- emacs/init.el
+++ emacs/init.el
@@ -12,6 +12,12 @@
 ;; https://www.emacswiki.org/emacs/ShowParenMode
 (show-paren-mode 1)
 
+;; https://www.emacswiki.org/emacs/FillColumnIndicator
+(global-display-fill-column-indicator-mode 1)
+
+;; https://www.emacswiki.org/emacs/LineNumbers
+(global-display-line-numbers-mode 1)
+
 ;; https://www.reddit.com/r/emacs/comments/643dkt/use_package_vs_require_and_maybe_some_sorrowful/dfz3mtx
 
 ;; https://github.com/raxod502/straight.el#getting-started
