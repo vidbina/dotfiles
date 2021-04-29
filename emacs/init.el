4827e82d5d8270063e4630452bebd8c40a036167
David Asabina <vid@bina.me>
2021-04-29 22:12:30 +0200

    feat: Install and conf Emacs org-roam-server

--- emacs/init.el
+++ emacs/init.el
@@ -224,6 +224,25 @@
   org-roam
   )
 
+(use-package org-roam-server
+  :straight
+  (org-roam-server
+    :type git
+    :host github
+    :repo "org-roam/org-roam-server")
+  :config
+  (setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 6003
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20))
+
 ;; https://github.com/dgutov/diff-hl
 (use-package diff-hl
   :straight
