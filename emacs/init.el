7767771f30624ca4a8e58ebbd2aac55fe7508390
David Asabina <vid@bina.me>
2021-08-06 13:36:50 +0200

    feat: Config Emacs org-babel plantuml

--- emacs/init.el
+++ emacs/init.el
@@ -213,9 +213,9 @@
   ;; https://orgmode.org/manual/Structure-Templates.html
   (load-library "org-tempo")
   ;; https://orgmode.org/worg/org-contrib/babel/languages/ob-doc-dot.html
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((dot . t))) ; this line activates dot
+  ;; activate dot
+  (setq org-plantuml-exec-mode 'plantuml)
+  (org-babel-do-load-languages 'org-babel-load-languages '((dot . t) (plantuml . t)))
   ;; https://www.reddit.com/r/emacs/comments/ldiryk/weird_tab_behavior_in_org_mode_source_blocks
   (setq org-src-preserve-indentation t)
   (setq
