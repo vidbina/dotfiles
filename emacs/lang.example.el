3b7acacd67d9386b499ed8f4b61e42b8e1af141b
David Asabina <vid@bina.me>
2021-10-17 03:35:19 +0200

    chore: Format Emacs config and comment LSP setup

--- emacs/lang.example.el
+++ emacs/lang.example.el
@@ -62,8 +62,8 @@
 ;; https://github.com/emacsmirror/paredit
 (use-package paredit
   :straight (paredit :type git
-                          :host github
-                          :repo "emacsmirror/paredit"))
+                     :host github
+                     :repo "emacsmirror/paredit"))
 
 ;; https://github.com/clojure-emacs/clojure-mode
 (use-package clojure-mode
@@ -95,26 +95,26 @@
   (clojure-mode . envrc-mode))
 
 ;; https://emacs-lsp.github.io/lsp-mode/page/installation/#vanilla-emacs
-(use-package lsp-mode
-  :straight (lsp-mode :type git
-                      :host github
-                      :repo "emacs-lsp/lsp-mode")
-  :bind (:map lsp-mode-map ("TAB" . completion-at-point))
-  :init
-  (setq-default read-process-output-max (* 1024 1024))
-  (setq  gc-cons-threshold (* 100 1024 1024))
-  ;;(setq lsp-keymap-prefix "C-c C-M-l")
-  ;;(setq lsp-log-io t) ;; ⚠️ turn off for performance
-  :hook
-  ;; (XXX-mode . lsp)
-  (java-mode . lsp)
-    (go-mode . lsp)
-    :commands
-    (lsp lsp-deferred)
-    ;;:config
-    ;;(define-key lsp-mode-map (kbd "C-c C-M-l") lsp-command-map)
-    ;;(lsp-enable-which-key-integration t)
-    )
+;;(use-package lsp-mode
+;;  :straight (lsp-mode :type git
+;;                      :host github
+;;                      :repo "emacs-lsp/lsp-mode")
+;;  :bind (:map lsp-mode-map ("TAB" . completion-at-point))
+;;  :init
+;;  (setq-default read-process-output-max (* 1024 1024))
+;;  (setq  gc-cons-threshold (* 100 1024 1024))
+;;  ;;(setq lsp-keymap-prefix "C-c C-M-l")
+;;  ;;(setq lsp-log-io t) ;; ⚠️ turn off for performance
+;;  :hook
+;;  ;; (XXX-mode . lsp)
+;;  (java-mode . lsp)
+;;  (go-mode . lsp)
+;;  :commands
+;;  (lsp lsp-deferred)
+;;  ;;:config
+;;  ;;(define-key lsp-mode-map (kbd "C-c C-M-l") lsp-command-map)
+;;  ;;(lsp-enable-which-key-integration t)
+;;  )
 
 ;; https://emacs-lsp.github.io/lsp-mode/page/installation/#use-package
 ;; https://config.daviwil.com/emacs
@@ -136,32 +136,32 @@
 ;;  :commands lsp-treemacs-errors-list
 ;;  :after lsp-mode)
 
-(setq lsp-docker-client-configs '((:server-id bash-ls :docker-server-id bashls-docker :server-command "bash-language-server start")
-                                  (:server-id clangd :docker-server-id clangd-docker :server-command "clangd")
-                                  (:server-id css-ls :docker-server-id cssls-docker :server-command "css-languageserver --stdio")
-                                  (:server-id dockerfile-ls :docker-server-id dockerfilels-docker :server-command "docker-langserver --stdio")
-                                  (:server-id gopls :docker-server-id gopls-docker :server-command "gopls")
-                                  (:server-id html-ls :docker-server-id htmls-docker :server-command "html-languageserver --stdio")
-                                  (:server-id pyls :docker-server-id pyls-docker :server-command "pyls")
-                                  (:server-id ts-ls :docker-server-id tsls-docker :server-command "typescript-language-server --stdio")))
+;;(setq lsp-docker-client-configs '((:server-id bash-ls :docker-server-id bashls-docker :server-command "bash-language-server start")
+;;                                  (:server-id clangd :docker-server-id clangd-docker :server-command "clangd")
+;;                                  (:server-id css-ls :docker-server-id cssls-docker :server-command "css-languageserver --stdio")
+;;                                  (:server-id dockerfile-ls :docker-server-id dockerfilels-docker :server-command "docker-langserver --stdio")
+;;                                  (:server-id gopls :docker-server-id gopls-docker :server-command "gopls")
+;;                                  (:server-id html-ls :docker-server-id htmls-docker :server-command "html-languageserver --stdio")
+;;                                  (:server-id pyls :docker-server-id pyls-docker :server-command "pyls")
+;;                                  (:server-id ts-ls :docker-server-id tsls-docker :server-command "typescript-language-server --stdio")))
 
-;; https://github.com/emacs-lsp/lsp-docker
-(use-package lsp-docker
-  :straight (lsp-docker :type git
-                        :host github
-                        :repo "emacs-lsp/lsp-docker")
-  :after lsp-mode
-  ;;:init
-  ;;(setq lsp-docker-client-packages '()
-  ;;      lsp-docker-client-configs '())
-  :config
-  (lsp-docker-init-clients :path-mappings '(("/home/vidbina/src" . "/projects"))
-                           ;;:docker-image-id "vidbina/lsp-docker-langservers:latest"
-                           :client-packages lsp-docker-default-client-packages
-                           :client-configs lsp-docker-default-client-configs
-                           ;;:client-configs '((:server-id 'clangd
-                           ;;                              :docker-server-id 'examplels-docker
-                           ;;                              :docker-image-id "vidbina/lsp-docker-langservers:latest"
-                           ;;                              :docker-container-name "vidbina-lsp-clangd"
-                           ;;                              :server-command "ccls"))
-                           ))
+;;;; https://github.com/emacs-lsp/lsp-docker
+;;(use-package lsp-docker
+;;  :straight (lsp-docker :type git
+;;                        :host github
+;;                        :repo "emacs-lsp/lsp-docker")
+;;  :after lsp-mode
+;;  ;;:init
+;;  ;;(setq lsp-docker-client-packages '()
+;;  ;;      lsp-docker-client-configs '())
+;;  :config
+;;  (lsp-docker-init-clients :path-mappings '(("/home/vidbina/src" . "/projects"))
+;;                           ;;:docker-image-id "vidbina/lsp-docker-langservers:latest"
+;;                           :client-packages lsp-docker-default-client-packages
+;;                           :client-configs lsp-docker-default-client-configs
+;;                           ;;:client-configs '((:server-id 'clangd
+;;                           ;;                              :docker-server-id 'examplels-docker
+;;                           ;;                              :docker-image-id "vidbina/lsp-docker-langservers:latest"
+;;                           ;;                              :docker-container-name "vidbina-lsp-clangd"
+;;                           ;;                              :server-command "ccls"
+;;                           ))
