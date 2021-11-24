b049dee0bf20e0fce5131846941ac74442b88757
David Asabina <vid@bina.me>
2021-11-24 16:30:21 +0700

    feat: Enable redshift-gammastep

--- home.nix
+++ home.nix
@@ -120,6 +120,17 @@
     };
   };
 
+  services.gammastep = {
+    enable = true;
+    dawnTime = "6:00-7:45";
+    duskTime = "18:35-20:15";
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+    tray = true;
+  };
+
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
