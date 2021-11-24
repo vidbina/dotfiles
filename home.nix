9745802f149daf4aa849afeb0d949d5650e7ebae
David Asabina <vid@bina.me>
2021-11-24 15:23:11 +0700

    feat: Install xsel-copy-url w/ related desktopItem

--- home.nix
+++ home.nix
@@ -16,6 +16,33 @@
   # changes in each release.
   home.stateVersion = "21.05";
 
+  home.packages = [
+    (pkgs.makeDesktopItem {
+      name = "xsel-copy-url";
+      exec = "${pkgs.xsel-copy-url}/bin/xsel-copy-url %U";
+      comment = "Open link by copying it into the clipboard with xsel";
+      desktopName = "xsel-copy-url";
+      type = "Application";
+      categories = [ "Network;WebBrowser;" ];
+      mimeType = builtins.concatStringsSep ";" [
+        "text/html"
+        "x-scheme-handler/http"
+        "x-scheme-handler/https"
+        "x-scheme-handler/ftp"
+      ];
+    })
+  ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+      xsel-copy-url = pkgs.writeScriptBin "xsel-copy-url" ''
+        url=$1
+        echo "$url" | ${pkgs.xsel}/bin/xsel -ib
+        ${pkgs.libnotify}/bin/notify-send --category=url --urgency=low "🌍 Link Copied" "Paste to enter $url"
+      '';
+    })
+  ];
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
