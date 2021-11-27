fb37d35026a6b6701c9a3bd492aaaa835efc7c9a
David Asabina <vid@bina.me>
2021-11-28 04:36:55 +0700

    feat: Define Emacs overlay and enable prog and svc

--- home.nix
+++ home.nix
@@ -94,6 +94,73 @@
             "vidbina-light.Xresources"
           ];
         };
+
+      my-emacs =
+        let
+          current-emacs = pkgs.emacs;
+          bundle = (pkgs.emacsPackagesNgGen current-emacs).emacsWithPackages;
+          bundled-emacs = bundle (epkgs: (
+            with epkgs; [
+              notmuch
+              vterm
+              pdf-tools
+            ]
+          ) ++ (
+            with epkgs.melpaStablePackages; [
+            ]
+          ) ++ (
+            with epkgs.melpaPackages; [
+            ]
+          ));
+          ripgrep-for-doom-emacs = (pkgs.ripgrep.override {
+            withPCRE2 = true;
+          });
+          jupyter-for-emacs = (pkgs.python38.withPackages (ps: with ps; [
+            jupyter
+          ]));
+        in
+        (pkgs.buildEnv {
+          name = "my-emacs";
+          paths = [
+            bundled-emacs
+            pkgs.clang
+            pkgs.cmake
+            pkgs.coreutils
+            pkgs.fd
+            pkgs.multimarkdown
+            jupyter-for-emacs
+            ripgrep-for-doom-emacs
+
+            # for Emacs
+            (pkgs.makeDesktopItem {
+              name = "org-protocol";
+              exec = "${bundled-emacs}/bin/emacsclient --create-frame %u";
+              comment = "Org Protocol";
+              desktopName = "org-protocol";
+              type = "Application";
+              mimeType = "x-scheme-handler/org-protocol";
+            })
+
+            # for Emacs
+            # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-0.9.5.html
+            # https://www.emacswiki.org/emacs/MailtoHandler
+            # https://dev.spacekookie.de/kookie/nomicon/commit/9e5896496cfd5da5754018887f7ad3b256b3ad80.diff
+            (pkgs.makeDesktopItem {
+              name = "emacs-mu4e";
+              exec = ''
+                ${bundled-emacs}/bin/emacsclient --create-frame --eval "(browse-url-mail \"%u\")"
+              '';
+              comment = "Emacs mu4e";
+              desktopName = "emacs-mu4e";
+              type = "Application";
+              mimeType = builtins.concatStringsSep ";" [
+                # Email
+                "x-scheme-handler/mailto"
+                "message/rfc822"
+              ];
+            })
+          ];
+        });
     })
   ];
 
@@ -112,6 +179,11 @@
     nix-direnv = {
       enable = true;
     };
+  };
+
+  programs.emacs = {
+    enable = true;
+    package = pkgs.my-emacs;
   };
 
   programs.neovim = {
@@ -215,6 +287,11 @@
       "M-C-l" = "perl:color-themes:load-state";
       "M-C-s" = "perl:color-themes:save-state";
     };
+  };
+
+  services.emacs = {
+    enable = true;
+    client.enable = true;
   };
 
   services.gammastep = {
