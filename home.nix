b6648a0d70b037b23baacee654f83c5ccffcf7ee
David Asabina <vid@bina.me>
2021-11-28 04:02:29 +0700

    feat: Bundle xsel-copy-url with desktopItem
    Bundle produces a single package within the overlay, named
    xsel-copy-url, which, when installed, delivers the xsel-copy-url script
    along with the desktopItem in a single go.

--- home.nix
+++ home.nix
@@ -17,21 +17,6 @@
   home.stateVersion = "21.05";
 
   home.packages = [
-    (pkgs.makeDesktopItem {
-      name = "xsel-copy-url";
-      exec = "${pkgs.xsel-copy-url}/bin/xsel-copy-url %U";
-      comment = "Open link by copying it into the clipboard with xsel";
-      desktopName = "xsel-copy-url";
-      type = "Application";
-      categories = [ "Network;WebBrowser;" ];
-      mimeType = builtins.concatStringsSep ";" [
-        "text/html"
-        "x-scheme-handler/http"
-        "x-scheme-handler/https"
-        "x-scheme-handler/ftp"
-      ];
-    })
-
     (pkgs.writeScriptBin "colors" ''
       # https://askubuntu.com/questions/27314/script-to-display-all-terminal-colors
 
@@ -57,11 +42,39 @@
 
   nixpkgs.overlays = [
     (self: super: {
-      xsel-copy-url = pkgs.writeScriptBin "xsel-copy-url" ''
-        url=$1
-        echo "$url" | ${pkgs.xsel}/bin/xsel -ib
-        ${pkgs.libnotify}/bin/notify-send --category=url --urgency=low "🌍 Link Copied" "Paste to enter $url"
-      '';
+      xsel-copy-url =
+        let
+          xsel-copy-url = pkgs.writeScriptBin "xsel-copy-url" ''
+            url=$1
+            echo "$url" | ${pkgs.xsel}/bin/xsel -ib
+            ${pkgs.libnotify}/bin/notify-send \
+              --category=url \
+              --urgency=low \
+              "🌍 Link Copied" "Paste to enter $url"
+          '';
+        in
+        pkgs.buildEnv {
+          name = "xsel-copy-url";
+          paths = [
+            xsel-copy-url
+
+            (pkgs.makeDesktopItem {
+              name = "xsel-copy-url";
+              exec = "${xsel-copy-url}/bin/xsel-copy-url %U";
+              comment = "Open link by copying it into the clipboard with xsel";
+              desktopName = "xsel-copy-url";
+              type = "Application";
+              categories = [ "Network;WebBrowser;" ];
+              mimeType = builtins.concatStringsSep ";" [
+                "text/html"
+                "x-scheme-handler/http"
+                "x-scheme-handler/https"
+                "x-scheme-handler/ftp"
+              ];
+            })
+          ];
+        };
+
       vidbina-urxvt-themes =
         let
           readTheme = x:
