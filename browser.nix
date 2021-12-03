8c415dbced6768581234195cfac75d91fb8af9ed
David Asabina <vid@bina.me>
2021-12-04 00:23:56 +0700

    feat: Add chromium extensions MetaMask and Vimium

--- browser.nix
+++ browser.nix
@@ -3,5 +3,18 @@
 {
   programs.chromium = {
     enable = true;
+
+    extensions = [
+      {
+        # Metamask
+        # https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn
+        id = "nkbihfbeogaeaoehlefnkodbefgpgknn";
+      }
+      {
+        # Vimium
+        # https://chrome.google.com/webstore/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb
+        id = "dbepggeogbaibhgnhhndojpepiihcmeb";
+      }
+    ];
   };
 }
