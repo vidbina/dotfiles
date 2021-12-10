0ba80143a41b4b927440ce72ec2fea5275d2caa0
David Asabina <vid@bina.me>
2021-12-10 23:56:25 +0700

    chore: Indent Emacs org-roam-ui conff + untheme

--- emacs/init.el
+++ emacs/init.el
@@ -235,31 +235,20 @@
 
 ;; https://github.com/org-roam/org-roam-ui
 (use-package org-roam-ui
-  :straight
-    (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
-    :after org-roam
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t)
-    (setq org-roam-ui-custom-theme
-    '((bg . "#1E2029")
-        (bg-alt . "#282a36")
-        (fg . "#f8f8f2")
-        (fg-alt . "#6272a4")
-        (red . "#ff5555")
-        (orange . "#f1fa8c")
-        (yellow ."#ffb86c")
-        (green . "#50fa7b")
-        (cyan . "#8be9fd")
-        (blue . "#ff79c6")
-        (violet . "#8be9fd")
-        (magenta . "#bd93f9"))))
+  :straight (org-roam-ui :host github
+                         :repo "org-roam/org-roam-ui"
+                         :branch "main"
+                         :files ("*.el" "out"))
+  :after org-roam
+  ;; normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;; a hookable mode anymore, you're advised to pick something yourself
+  ;; if you don't care about startup time, use
+  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
 
 ;; https://github.com/dgutov/diff-hl
 (use-package diff-hl
