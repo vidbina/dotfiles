52d6a832d36e3eb7df5741e5449eb895dfb52cf9
David Asabina <vid@bina.me>
2021-12-26 14:48:31 +0000

    feat: Install TeXlive packages

---
+++ doc.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+let
+  texlive-asabina = with pkgs; (texlive.combine {
+    inherit (texlive)
+      scheme-medium
+      luatex
+
+      atenddvi
+      IEEEtran
+      background
+      bashful
+      capt-of
+      collection-basic
+      collection-fontsrecommended
+      collection-langeuropean
+      collection-langgerman
+      collection-latexrecommended
+      datetime
+      draftwatermark
+      enumitem
+      eso-pic
+      etoolbox
+      everypage
+      fmtcount
+      lastpage
+      latexdiff
+      mdframed
+      needspace
+      numprint
+      paracol
+      pdfcrop
+      pgfgantt
+      soul
+      tableof
+      titlepic
+      tocloft
+      ulem
+      wrapfig
+      xargs
+      xetex
+      xstring
+      xtab
+      ;
+  });
+in
+{
+  home.packages = with pkgs; [
+    aspell
+    aspellDicts.de
+    aspellDicts.en
+    aspellDicts.nl
+    bibtex2html
+    biber
+    evince
+    libreoffice
+    okular
+    texlive-asabina
+    pdftk
+    scim
+    visidata
+    xournal
+    zathura
+  ];
+}
--- home.nix
+++ home.nix
@@ -6,6 +6,7 @@ let
 in
 {
   imports = [
+    ./doc.nix
     ./browser.nix
     ./x.nix
 
