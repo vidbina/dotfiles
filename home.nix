9661a66eb6c033bee82a0b2bd77e57c65cfe7f65
David Asabina <vid@bina.me>
2022-01-25 20:19:50 +0000

    fix: Circumvent init.vim and config nvim in nix

--- home.nix
+++ home.nix
@@ -53,12 +53,53 @@ in
 
   programs.neovim = {
     enable = true;
-    extraConfig = builtins.readFile (./. + "/nvim/init.vim");
-    plugins = with pkgs.vimPlugins; [
-      {
-        plugin = vim-plug;
-        optional = false;
-      }
+    #extraConfig = builtins.readFile (./. + "/nvim/init.vim");
+    plugins = with pkgs.vimPlugins; let
+      #sentinel-vim = pkgs.vimUtils.buildVimPlugin {
+      #  name = "sentinel-vim";
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "hashicorp";
+      #    repo = "sentinel.vim";
+      #    rev = "main";
+      #    sha256 = pkgs.lib.fakeSha256;
+      #  };
+      #};
+    in
+    [
+      #{
+      #  plugin = vim-plug;
+      #  optional = false;
+      #}
+
+      #'https://github.com/hrother/offlineimaprc.vim.git'
+      #'isRuslan/vim-es6'
+      #'jeffkreeftmeijer/vim-dim', { 'branch': 'main' }
+      #'sigmike/vim-taskjuggler'
+      #'vim-scripts/openvpn'
+      #ale
+      #vim-scala
+      coc-nvim
+      elm-vim
+      goyo-vim
+      neoformat
+      nerdtree
+      nvim-treesitter
+      orgmode
+      plantuml-syntax
+      #sentinel-vim
+      tabular
+      tagbar
+      typescript-vim
+      vim-airline
+      vim-fugitive
+      vim-gitgutter
+      vim-graphql
+      vim-markdown
+      vim-nix
+      vim-prettier
+      vim-solidity
+      vim-terraform
+      wmgraphviz-vim
     ];
     vimdiffAlias = true;
     withRuby = true;
@@ -135,7 +176,7 @@ in
     #dotDir = ".config/zsh";
 
     initExtraBeforeCompInit = ''
-      export EDITOR=nvim
+      export EDITOR="emacsclient -c -a emacs"
       export VISUAL="emacsclient -c -a emacs"
 
       setopt histignorespace # keeps lines preceded with SPACE out of history
