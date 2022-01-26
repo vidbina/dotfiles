2d36c1932d1cb2da60849902292b8dc9d3d1a563
David Asabina <vid@bina.me>
2022-01-26 09:17:45 +0000

    refactor: Isolate NeoVim config

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@ in
     ./browser.nix
     ./x.nix
     ./mail.nix
+    ./vim.nix
 
     ./rofi
 
@@ -49,60 +50,6 @@ in
     nix-direnv = {
       enable = true;
     };
-  };
-
-  programs.neovim = {
-    enable = true;
-    #extraConfig = builtins.readFile (./. + "/nvim/init.vim");
-    plugins = with pkgs.vimPlugins; let
-      #sentinel-vim = pkgs.vimUtils.buildVimPlugin {
-      #  name = "sentinel-vim";
-      #  src = pkgs.fetchFromGitHub {
-      #    owner = "hashicorp";
-      #    repo = "sentinel.vim";
-      #    rev = "main";
-      #    sha256 = pkgs.lib.fakeSha256;
-      #  };
-      #};
-    in
-    [
-      #{
-      #  plugin = vim-plug;
-      #  optional = false;
-      #}
-
-      #'https://github.com/hrother/offlineimaprc.vim.git'
-      #'isRuslan/vim-es6'
-      #'jeffkreeftmeijer/vim-dim', { 'branch': 'main' }
-      #'sigmike/vim-taskjuggler'
-      #'vim-scripts/openvpn'
-      #ale
-      #vim-scala
-      coc-nvim
-      elm-vim
-      goyo-vim
-      neoformat
-      nerdtree
-      nvim-treesitter
-      orgmode
-      plantuml-syntax
-      #sentinel-vim
-      tabular
-      tagbar
-      typescript-vim
-      vim-airline
-      vim-fugitive
-      vim-gitgutter
-      vim-graphql
-      vim-markdown
-      vim-nix
-      vim-prettier
-      vim-solidity
-      vim-terraform
-      wmgraphviz-vim
-    ];
-    vimdiffAlias = true;
-    withRuby = true;
   };
 
   programs.tmux = {
---
+++ vim.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    # Warning: Just bailed on init.vim and opted for nix so, WIP!
+
+    #extraConfig = builtins.readFile (./. + "/nvim/init.vim");
+    plugins = with pkgs.vimPlugins; let
+      #sentinel-vim = pkgs.vimUtils.buildVimPlugin {
+      #  name = "sentinel-vim";
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "hashicorp";
+      #    repo = "sentinel.vim";
+      #    rev = "main";
+      #    sha256 = pkgs.lib.fakeSha256;
+      #  };
+      #};
+    in
+    [
+      #{
+      #  plugin = vim-plug;
+      #  optional = false;
+      #}
+
+      #'https://github.com/hrother/offlineimaprc.vim.git'
+      #'isRuslan/vim-es6'
+      #'jeffkreeftmeijer/vim-dim', { 'branch': 'main' }
+      #'sigmike/vim-taskjuggler'
+      #'vim-scripts/openvpn'
+      #ale
+      #vim-scala
+      coc-nvim
+      elm-vim
+      goyo-vim
+      neoformat
+      nerdtree
+      nvim-treesitter
+      orgmode
+      plantuml-syntax
+      #sentinel-vim
+      tabular
+      tagbar
+      typescript-vim
+      vim-airline
+      vim-fugitive
+      vim-gitgutter
+      vim-graphql
+      vim-markdown
+      vim-nix
+      vim-prettier
+      vim-solidity
+      vim-terraform
+      wmgraphviz-vim
+    ];
+    vimdiffAlias = true;
+    withRuby = true;
+  };
+}
