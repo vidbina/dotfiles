13de01b6e1479f655959ea1d4074e776f50ff2fa
David Asabina <vid@bina.me>
2022-01-28 17:11:38 +0000

    refactor: Split Emacs language config

--- emacs/lang.example.el
+++ emacs/lang.example.el
@@ -16,6 +16,21 @@
   (unless (file-exists-p ispell-personal-dictionary)
     (write-region "" nil ispell-personal-dictionary nil 0)))
 
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Hooks-for-Loading.html
+;; https://orgmode.org/worg/org-contrib/babel/languages/ob-doc-gnuplot.html#sec-4
+(with-eval-after-load 'org
+  (message "Loading org-babel-language mappings")
+  (org-babel-do-load-languages 'org-babel-load-languages
+                               '((shell . t)
+                                 (clojure .  t)
+                                 (dot . t)
+                                 (gnuplot . t)
+                                 (haskell . t)
+                                 (makefile . t)
+                                 ;; (nix . t) ;; TODO: Figure out why broken
+                                 (plantuml . t)
+                                 (python . t))))
+
 ;; https://jblevins.org/projects/markdown-mode/
 (use-package markdown-mode
   :straight (markdown-mode :type git
@@ -79,12 +94,6 @@
                       :host github
                       :repo "emacs-lsp/lsp-java"))
 
-;; https://github.com/emacsmirror/paredit
-(use-package paredit
-  :straight (paredit :type git
-                     :host github
-                     :repo "emacsmirror/paredit"))
-
 ;; https://github.com/clojure-emacs/clojure-mode
 (use-package clojure-mode
   :straight (clojure-mode :type git
@@ -105,15 +114,6 @@
                         :host github
                         :repo "swift-emacs/swift-mode"))
 
-;;;; https://github.com/clojure-emacs/cider
-;;(use-package cider
-;;  :straight (cider :type git
-;;                   :host github
-;;                   :repo "clojure-emacs/cider")
-;;  :config
-;;  (setq org-babel-clojure-backend 'cider
-;;        cider-lein-parameters "with-profile -user repl :headless :host localhost"))
-
 ;; https://github.com/haskell/haskell-mode
 (use-package haskell-mode
   :straight (haskell-mode :type git
@@ -121,6 +121,12 @@
                           :repo "haskell/haskell-mode")
   :init
   (add-hook 'haskell-mode-hook 'haskell-unicode-input-method-enable))
+
+;; https://github.com/emacsmirror/paredit
+(use-package paredit
+  :straight (paredit :type git
+                     :host github
+                     :repo "emacsmirror/paredit"))
 
 ;; https://github.com/purcell/inheritenv
 (use-package inheritenv
@@ -141,98 +147,3 @@
   :straight (eglot :type git
                    :host github
                    :repo "joaotavora/eglot"))
-
-;; eldoc.el / eldoc-display-message-no-interference-p
-;;   refers to
-;; paren.el / show-paren--overlay and
-;; paren.el / show-paren-context-when-offscreen
-;;   which are bleeding edge
-
-;; https://github.com/emacs-mirror/emacs/commit/9f505c476eb1a8e85ba26964abf218cab7db0e57
-
-;; https://emacs-lsp.github.io/lsp-mode/page/installation/#vanilla-emacs
-;;(use-package lsp-mode
-;;  :straight (lsp-mode :type git
-;;                      :host github
-;;                      :repo "emacs-lsp/lsp-mode")
-;;  :bind (:map lsp-mode-map ("TAB" . completion-at-point))
-;;  :init
-;;  (setq-default read-process-output-max (* 1024 1024))
-;;  (setq  gc-cons-threshold (* 100 1024 1024))
-;;  ;;(setq lsp-keymap-prefix "C-c C-M-l")
-;;  ;;(setq lsp-log-io t) ;; ⚠️ turn off for performance
-;;  :hook
-;;  ;; (XXX-mode . lsp)
-;;  (java-mode . lsp)
-;;  (go-mode . lsp)
-;;  :commands
-;;  (lsp lsp-deferred)
-;;  ;;:config
-;;  ;;(define-key lsp-mode-map (kbd "C-c C-M-l") lsp-command-map)
-;;  ;;(lsp-enable-which-key-integration t)
-;;  )
-
-;; https://emacs-lsp.github.io/lsp-mode/page/installation/#use-package
-;; https://config.daviwil.com/emacs
-;; https://github.com/emacs-lsp/lsp-ui
-;;(use-package lsp-ui
-;;  ;;:straight t
-;;  :hook (lsp-mode . lsp-ui-mode)
-;;  :config
-;;  (setq lsp-ui-sideline-enable t
-;;        lsp-ui-sideline-show-hover nil
-;;        lsp-ui-doc-position 'bottom)
-;;  (lsp-ui-doc-show))
-
-;;;; if you are ivy user
-;;(use-package lsp-ivy
-;;  :hook (lsp-mode . lsp-ivy-mode))
-
-;;(use-package lsp-treemacs
-;;  :commands lsp-treemacs-errors-list
-;;  :after lsp-mode)
-
-;;(setq lsp-docker-client-configs '((:server-id bash-ls :docker-server-id bashls-docker :server-command "bash-language-server start")
-;;                                  (:server-id clangd :docker-server-id clangd-docker :server-command "clangd")
-;;                                  (:server-id css-ls :docker-server-id cssls-docker :server-command "css-languageserver --stdio")
-;;                                  (:server-id dockerfile-ls :docker-server-id dockerfilels-docker :server-command "docker-langserver --stdio")
-;;                                  (:server-id gopls :docker-server-id gopls-docker :server-command "gopls")
-;;                                  (:server-id html-ls :docker-server-id htmls-docker :server-command "html-languageserver --stdio")
-;;                                  (:server-id pyls :docker-server-id pyls-docker :server-command "pyls")
-;;                                  (:server-id ts-ls :docker-server-id tsls-docker :server-command "typescript-language-server --stdio")))
-
-;;;; https://github.com/emacs-lsp/lsp-docker
-;;(use-package lsp-docker
-;;  :straight (lsp-docker :type git
-;;                        :host github
-;;                        :repo "emacs-lsp/lsp-docker")
-;;  :after lsp-mode
-;;  ;;:init
-;;  ;;(setq lsp-docker-client-packages '()
-;;  ;;      lsp-docker-client-configs '())
-;;  :config
-;;  (lsp-docker-init-clients :path-mappings '(("/home/vidbina/src" . "/projects"))
-;;                           ;;:docker-image-id "vidbina/lsp-docker-langservers:latest"
-;;                           :client-packages lsp-docker-default-client-packages
-;;                           :client-configs lsp-docker-default-client-configs
-;;                           ;;:client-configs '((:server-id 'clangd
-;;                           ;;                              :docker-server-id 'examplels-docker
-;;                           ;;                              :docker-image-id "vidbina/lsp-docker-langservers:latest"
-;;                           ;;                              :docker-container-name "vidbina-lsp-clangd"
-;;                           ;;                              :server-command "ccls"
-;;                           ))
-
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Hooks-for-Loading.html
-;; https://orgmode.org/worg/org-contrib/babel/languages/ob-doc-gnuplot.html#sec-4
-(with-eval-after-load 'org
-  (message "Loading org-babel-language mappings")
-  (org-babel-do-load-languages 'org-babel-load-languages
-                               '((shell . t)
-                                 (clojure .  t)
-                                 (dot . t)
-                                 (gnuplot . t)
-                                 (haskell . t)
-                                 (makefile . t)
-                                 ;; (nix . t) ;; TODO: Figure out why broken
-                                 (plantuml . t)
-                                 (python . t))))
