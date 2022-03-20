ee152f3e7cfbe9e84dfc2e709c5ab9f8dd9f4f8a
David Asabina <vid@bina.me>
2022-03-20 22:55:33 +0000

    chore: Move mu installation into emacs config

--- mail.nix
+++ mail.nix
@@ -5,20 +5,6 @@
     isync
     mb2md
     msmtp
-    (mu.overrideAttrs (oldAttrs:
-      let
-        rev = "bbf55256e58aa62546e8bdade1d127d7e6a9b57e";
-      in
-      {
-        version = "1.6.10-${rev}";
-        src = fetchFromGitHub {
-          owner = "djcb";
-          repo = "mu";
-          rev = "${rev}";
-          sha256 = "sha256-ozIITQbt7U4qDzHjbfDyIogIkMRpX1VsBr9igdpNqcI=";
-        };
-        emacs = my-emacs;
-      }))
     neomutt
     notmuch
     notmuch-mutt
