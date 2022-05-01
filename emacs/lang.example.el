a4f9f6b32f910ea4997fbf4d81945078fcd1d1a3
David Asabina <vid@bina.me>
2022-05-01 19:23:15 +0000

    chore: tangle refactor of :after org + :hook configs for Emacs langs

--- emacs/lang.example.el
+++ emacs/lang.example.el
@@ -62,11 +62,14 @@
 
 ;; https://github.com/skuro/plantuml-mode
 (use-package plantuml-mode
-  :after org
   :straight (plantuml-mode :type git
                            :host github
                            :repo "skuro/plantuml-mode")
+  :after org
   :config
+  ;; https://orgmode.org/worg/org-contrib/babel/languages/ob-doc-dot.html
+  (setq org-plantuml-exec-mode 'plantuml)
+
   (setq plantuml-default-exec-mode 'executable)
   (org-babel-do-load-languages 'org-babel-load-languages
                                (append org-babel-load-languages
@@ -74,10 +77,10 @@
 
 ;; https://github.com/ppareit/graphviz-dot-mode
 (use-package graphviz-dot-mode
-  :after org
   :straight (graphviz-dot-mode :type git
                                :host github
                                :repo "ppareit/graphviz-dot-mode")
+  :after org
   :config
   (setq graphviz-dot-indent-width 2)
   (org-babel-do-load-languages 'org-babel-load-languages
@@ -88,10 +91,10 @@
 ;; also https://github.com/bruceravel/gnuplot-mode
 ;; also https://github.com/rudi/gnuplot-el
 (use-package gnuplot
-  :after org
   :straight (gnuplot :type git
                      :host github
                      :repo "emacsorphanage/gnuplot")
+  :after org
   :config
   (org-babel-do-load-languages 'org-babel-load-languages
                                (append org-babel-load-languages
@@ -128,10 +131,8 @@
   :straight (js-comint :type git
                        :host github
                        :repo "redguardtoo/js-comint")
-  ;;:hook
-  ;;(inferior-js-mode . (lambda ()
-  ;;                      (add-hook 'inferior-js-mode-hook
-  ;;                                'inferior-js-mode-hook-setup t)))
+  :hook (inferior-js-mode . (lambda ()
+                              (add-hook 'comint-output-filter-functions 'js-comint-process-output)))
   :config
   (define-key js-mode-map [remap eval-last-sexp] #'js-comint-send-last-sexp)
   (define-key js-mode-map (kbd "C-c b") 'js-send-buffer))
@@ -170,9 +171,8 @@
   :config
   (require 'haskell-interactive-mode)
   (require 'haskell-process)
-  :hook
-  (haskell-mode . haskell-unicode-input-method-enable)
-  (haskell-mode . interactive-haskell-mode)
+  :hook ((haskell-mode . haskell-unicode-input-method-enable)
+         (haskell-mode . interactive-haskell-mode))
   :custom
   (haskell-process-suggest-remove-import-lines t)
   (haskell-process-auto-import-loaded-modules t)
@@ -190,11 +190,12 @@
   :straight (rainbow-delimiters :type git
                                 :host github
                                 :repo "Fanael/rainbow-delimiters")
-  ;; :hook
+  :hook ((emacs-lisp-mode . rainbow-delimiters-mode)
+         (prog-mode . rainbow-delimiters-mode))
+
   ;; ;; https://github.com/patrickt/emacs
   ;; ((prog-mode) . rainbow-delimiters-mode)
-  :config
-  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode))
+  )
 
 ;; https://github.com/emacsmirror/paredit
 (use-package paredit
