10427847d3480b48686feea3564f3fce49e66dd6
David Asabina <vid@bina.me>
2022-07-10 14:48:44 +0000

    chore: move ispell config into use-package form

--- emacs/lang.example.el
+++ emacs/lang.example.el
@@ -5,18 +5,20 @@
                           :repo "emacsmirror/rainbow-mode"))
 
 ;; https://200ok.ch/posts/2020-08-22_setting_up_spell_checking_with_multiple_dictionaries.html
-(with-eval-after-load "ispell"
-  (setq ispell-program-name "hunspell")
+(use-package ispell
+  :straight (:type built-in)
+  :custom
+  (ispell-program-name "hunspell")
   ;; Configure German, Swiss German, and two variants of English.
-  (setq ispell-dictionary "en_US,de_DE,nl")
+  (ispell-dictionary "en_US,de_DE,nl")
+  ;; For saving words to the personal dictionary, don't infer it from
+  ;; the locale, otherwise it would save to ~/.hunspell_de_DE.
+  (ispell-personal-dictionary "~/.hunspell_personal")
+  :config
   ;; ispell-set-spellchecker-params has to be called
   ;; before ispell-hunspell-add-multi-dic will work
   (ispell-set-spellchecker-params)
   (ispell-hunspell-add-multi-dic ispell-dictionary)
-  ;; For saving words to the personal dictionary, don't infer it from
-  ;; the locale, otherwise it would save to ~/.hunspell_de_DE.
-  (setq ispell-personal-dictionary "~/.hunspell_personal")
-
   ;; The personal dictionary file has to exist, otherwise hunspell will
   ;; silently not use it.
   (unless (file-exists-p ispell-personal-dictionary)
