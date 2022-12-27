92a24df9fec27fe0f253aab79978e672ba5b904c
David Asabina <vid@bina.me>
2022-12-27 10:43:41 +0000

    chore: make tide-mode more web (JSX,TSX) savvy

--- emacs/lang.example.el
+++ emacs/lang.example.el
@@ -222,6 +222,25 @@
   (tide :type git
         :host github
         :repo "ananthakumaran/tide")
+  :config
+  ;; https://github.com/ananthakumaran/tide#tsx
+  (require 'web-mode)
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode)
+  ;; https://github.com/ananthakumaran/tide#jsx
+  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "jsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+  ;; configure jsx-tide checker to run after your default jsx checker
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
   :init
   (evil-collection-tide-setup)
   :delight
