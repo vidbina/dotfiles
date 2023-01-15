dc61cbb3cd040d7ba2495276d1f760b7cc8bd65e
David Asabina <vid@bina.me>
2023-01-15 12:51:41 +0000

    chore: temporarily disable package tree-sitter to use built-in

--- emacs/lang.example.el
+++ emacs/lang.example.el
@@ -318,25 +318,6 @@
   ;; ((prog-mode) . rainbow-delimiters-mode)
   )
 
-;; https://github.com/emacs-tree-sitter/elisp-tree-sitter
-(use-package tree-sitter
-  :straight (tree-sitter :type git
-                         :host github
-                         :repo "emacs-tree-sitter/elisp-tree-sitter")
-  :config
-  ;; activate tree-sitter on any buffer containing code for which it has a parser available
-  (global-tree-sitter-mode)
-  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
-  ;; by switching on and off
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-
-;; https://github.com/emacs-tree-sitter/tree-sitter-langs
-(use-package tree-sitter-langs
-  :straight (tree-sitter-langs :type git
-                               :host github
-                               :repo "emacs-tree-sitter/tree-sitter-langs")
-  :after tree-sitter)
-
 ;; https://github.com/DarthFennec/highlight-indent-guides
 (use-package highlight-indent-guides
   :straight (highlight-indent-guides :type git
