0b47d5a9f38fae572d98a9bf7b6efc150f88a70f
David Asabina <vid@bina.me>
2023-05-23 17:48:00 +0000

    chore: Update gammastep to Berlin

--- home-linux.nix
+++ home-linux.nix
@@ -29,8 +29,8 @@
     enable = true;
     dawnTime = "5:00-6:00";
     duskTime = "17:35-19:00";
-    latitude = 13.7;
-    longitude = 100.5;
+    latitude = 52.5;
+    longitude = 13.4;
 
     temperature = {
       # https://www.eizo.com/library/basics/color_temperature_on_an_LCD_monitor/
