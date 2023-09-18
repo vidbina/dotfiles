ce85d20c10b171f6c421f51f374e72a7c6f23a25
David Asabina <vid@bina.me>
2023-09-18 15:44:25 +0900

    chore: Config git with hm (CHANGE NOT OBSERVED)

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -18,6 +18,36 @@
           home.username = "vidbina";
           #home.homeDirectory = "/Users/vidbina";
           home.stateVersion = "23.05";
+
+          # NOTE: Copied from dev.nix
+          # No corresponding option in nix-darwin, so we config this with hm
+          programs.git = {
+            enable = true;
+            userName = "David Asabina";
+            userEmail = "vid@bina.me";
+            lfs.enable = true;
+            extraConfig = {
+              init = {
+                defaultBranch = "main";
+              };
+
+              core = {
+                editor = "nvim";
+              };
+
+              gpg = {
+                program = "gpg2";
+              };
+
+              # NOTE: Commenting out to test if `git config -l` changes
+              # sendemail = {
+              #   annotate = true;
+              #   smtpServer = "msmtp";
+              #   smtpServerOption = "-a vidbina";
+              # };
+            };
+          };
+
         }; # // import ../home-darwin.nix { inherit pkgs; lib = {}; };
       };
 
