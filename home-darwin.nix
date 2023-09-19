7c5f41bc3196f99a6b5e61aa26a20bdf0f2ed806
David Asabina <vid@bina.me>
2023-09-19 19:18:39 +0900

    chore: Workaround permission issue for ~/.emacs.d

--- home-darwin.nix
+++ home-darwin.nix
@@ -5,6 +5,16 @@
 
   home.packages = with pkgs; [ qemu ];
 
+
+  # home.file.".emacs.d".source = config.lib.file.mkOutOfStoreSymlink ./emacs;
+  # TODO: Fix hack of hardcoded dotfiles path
+  # NOTE: This repo must be checked out to ~/Code/vidbina/dotfiles
+  # A hardcoded .emacs.d source is used because mkOutOfStoreSymlink ./emacs
+  # does not seem to work on macOS.
+  # See https://discourse.nixos.org/t/accessing-home-manager-config-in-flakes/19864/8
+  # See https://github.com/nix-community/home-manager/issues/2085#issuecomment-861427318
+  home.file.".emacs.d".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/vidbina/dotfiles/emacs";
+
   # NOTE: Copied from dev.nix
   # No corresponding option in nix-darwin, so we config this with hm
   programs.git = {
