fe5800bbaf5bdf457ffe092ed42dd02bd9ffaf3e
David Asabina <vid@bina.me>
2023-09-19 11:50:56 +0900

    chore: Install neovim in hm config

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -33,4 +33,38 @@
       # };
     };
   };
+
+  # NOTE: Copied from vim.nix
+  programs.neovim = {
+    enable = true;
+    # Warning: Just bailed on init.vim and opted for nix so, WIP!
+    plugins = with pkgs.vimPlugins; [
+      coc-nvim
+      deoplete-notmuch
+      elm-vim
+      goyo-vim
+      neoformat
+      nerdtree
+      nvim-treesitter
+      orgmode
+      plantuml-syntax
+      sentinel-vim
+      tabular
+      tagbar
+      typescript-vim
+      vim-airline
+      vim-fugitive
+      vim-gitgutter
+      vim-graphql
+      vim-markdown
+      vim-nix
+      vim-prettier
+      vim-solidity
+      vim-terraform
+      wmgraphviz-vim
+    ];
+    vimdiffAlias = true;
+    withRuby = true;
+  };
+
 }
