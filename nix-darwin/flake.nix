9a544b7faa730d9e8d7898b8be705dd6af1047f1
David Asabina <vid@bina.me>
2023-09-19 12:04:58 +0900

    chore: Inline home-manager config

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,6 +1,6 @@
 # TODO: Tangle parts of the copied bits from source
 {
-  description = "Example Darwin system flake";
+  description = "David's Darwin (macOS) system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -10,25 +10,21 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-    let
-      hmConfig = {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.vidbina = import ./home.nix;
-      };
-    in
-    {
-      # Build with: nix run nix-darwin -- switch --flake ./nix-darwin/ --show-trace
-      darwinConfigurations."Davids-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-        # Specify ./configuration.nix as module and pass inputs as arg
-        # See https://github.com/LnL7/nix-darwin#using-flake-inputs
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          home-manager.darwinModules.home-manager
-          hmConfig
-        ];
-      };
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }: {
+    # Build with: nix run nix-darwin -- switch --flake ./nix-darwin/ --show-trace
+    darwinConfigurations."Davids-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      # Specify ./configuration.nix as module and pass inputs as arg
+      # See https://github.com/LnL7/nix-darwin#using-flake-inputs
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.vidbina = import ./home.nix;
+        }
+      ];
     };
+  };
 }
