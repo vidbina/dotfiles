e55f322152368519df9d3a85a90b231b06edb766
David Asabina <vid@bina.me>
2023-09-19 11:43:56 +0900

    chore: Isolate home-manager config

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -15,42 +15,7 @@
       hmConfig = {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.users.vidbina = {
-          home.username = "vidbina";
-          home.stateVersion = "23.05";
-
-          home.packages = [ ];
-
-          # NOTE: Copied from dev.nix
-          # No corresponding option in nix-darwin, so we config this with hm
-          programs.git = {
-            enable = true;
-            userName = "David Asabina";
-            userEmail = "vid@bina.me";
-            lfs.enable = true;
-            extraConfig = {
-              init = {
-                defaultBranch = "main";
-              };
-
-              core = {
-                editor = "nvim";
-              };
-
-              gpg = {
-                program = "gpg2";
-              };
-
-              # NOTE: Commenting out to test if `git config -l` changes
-              # sendemail = {
-              #   annotate = true;
-              #   smtpServer = "msmtp";
-              #   smtpServerOption = "-a vidbina";
-              # };
-            };
-          };
-
-        }; # // import ../home-darwin.nix { inherit pkgs; lib = {}; };
+        home-manager.users.vidbina = import ./home.nix;
       };
 
       # TODO: Isolate into separate file
---
+++ nix-darwin/home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.username = "vidbina";
+  home.stateVersion = "23.05";
+
+  home.packages = [ ];
+
+  # NOTE: Copied from dev.nix
+  # No corresponding option in nix-darwin, so we config this with hm
+  programs.git = {
+    enable = true;
+    userName = "David Asabina";
+    userEmail = "vid@bina.me";
+    lfs.enable = true;
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+
+      core = {
+        editor = "nvim";
+      };
+
+      gpg = {
+        program = "gpg2";
+      };
+
+      # NOTE: Commenting out to test if `git config -l` changes
+      # sendemail = {
+      #   annotate = true;
+      #   smtpServer = "msmtp";
+      #   smtpServerOption = "-a vidbina";
+      # };
+    };
+  };
+}
