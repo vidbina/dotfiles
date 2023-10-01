61d0f209511ceb772069b7ac03a055bf90ded816
David Asabina <vid@bina.me>
2023-10-01 21:29:57 +0900

    chore: Install vscode dev containers extension

--- home-darwin.nix
+++ home-darwin.nix
@@ -105,6 +105,7 @@
         mkhl.direnv
         ms-azuretools.vscode-docker
         ms-python.python
+        ms-vscode-remote.remote-containers
         vscode-org-mode.org-mode
         vscodevim.vim
       ];
