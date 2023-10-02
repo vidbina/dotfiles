e15ba9459ec6a27388d0f3fe39f92c5adab06a1c
David Asabina <vid@bina.me>
2023-10-02 22:20:30 +0900

    chore: Uninstall yabai and try Amethyst

--- system/darwin/wm.nix
+++ system/darwin/wm.nix
@@ -1,16 +1,5 @@
 { pkgs, lib, ... }: {
-  services.yabai = {
-    enable = true;
-    # config = {
-    #   focus_follows_mouse = "autoraise";
-    #   mouse_follows_focus = "off";
-    #   window_placement = "second_child";
-    #   window_opacity = "off";
-    #   top_padding = 36;
-    #   bottom_padding = 10;
-    #   left_padding = 10;
-    #   right_padding = 10;
-    #   window_gap = 10;
-    # };
-  };
+  homebrew.casks = [
+    "amethyst"
+  ];
 }
