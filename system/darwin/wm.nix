fbafec1c9b45ec8d461e91f3b9fb31eec095c356
David Asabina <vid@bina.me>
2023-10-03 15:27:47 +0900

    doc: List workaround to reorder wins in Amethyst

--- system/darwin/wm.nix
+++ system/darwin/wm.nix
@@ -1,20 +1,25 @@
 { pkgs, lib, ... }: {
   homebrew.casks = [
-    # NOTE: The follow keybindings are useful to remember:
-    # - Option + Shift + T = Toggle float for focused window
-    # - Option + Shift + J = Shift focus counterclockwise
-    # - Option + Shift + K = Shift focus clockwise
-    # - Option + Shift + M = Shift focus to main window
-    # - Option + Shift + D = Toggle between Fullscreen layout
-    # - Option + Shift + H = Shrink main pane
-    # - Option + Shift + L = Expand main pane
-    # - Option + Shift + S = Toggle between Wide layout
-    # - Option + Shift + Z = Reeval/recalc windows
-    # - Option + Shift + W = Focus Screen 1
-    # - Option + Shift + E = Focus Screen 2
-    # - Option + Shift + R = Focus Screen 3
-    # - Control + Option + Shift + Z = Relaunch Amethyst
-    # - Control + Option + Shift + Number = Throw focused window to space matching number
+    # NOTE: Mod1 is: Option + Shift
+    # - Mod1 + Space = Cycle layout (use Mod2 to reverse order)
+    # - Mod1 + Enter = Promote focused window to main window
+    # - Mod1 + T = Toggle float for focused window
+    # - Mod1 + J = Shift focus counterclockwise
+    # - Mod1 + K = Shift focus clockwise
+    # - Mod1 + M = Shift focus to main window
+    # - Mod1 + D = Toggle between Fullscreen layout
+    # - Mod1 + H = Shrink main pane
+    # - Mod1 + L = Expand main pane
+    # - Mod1 + S = Toggle between Wide layout
+    # - Mod1 + Z = Reeval/recalc windows
+    # - Mod1 + W = Focus Screen 1
+    # - Mod1 + E = Focus Screen 2
+    # - Mod1 + R = Focus Screen 3
+    # NOTE: Mod2 is: Control + Option + Shift
+    # - Mod2 + Z = Relaunch Amethyst
+    # - Mod2 + Number = Throw focused window to space matching number
+
+    # NOTE: Amethyst does support changing of window order, so use Mod1+Enter
     "amethyst"
   ];
 }
