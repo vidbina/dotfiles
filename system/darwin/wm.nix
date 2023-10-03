a301c4f0c097efabdefbfa690e27c7b5c9e42f7a
David Asabina <vid@bina.me>
2023-10-03 13:23:56 +0900

    doc: List key shortcuts for Amethyst

--- system/darwin/wm.nix
+++ system/darwin/wm.nix
@@ -1,5 +1,20 @@
 { pkgs, lib, ... }: {
   homebrew.casks = [
+    # NOTE: The follow keybindings are useful to remember:
+    # - Option + Shift + T = Toggle float for focused window
+    # - Option + Shift + J = Shift focus counterclockwise
+    # - Option + Shift + K = Shift focus clockwise
+    # - Option + Shift + M = Shift focus to main window
+    # - Option + Shift + D = Toggle between Fullscreen layout
+    # - Option + Shift + H = Shrink main pane
+    # - Option + Shift + L = Expand main pane
+    # - Option + Shift + S = Toggle between Wide layout
+    # - Option + Shift + Z = Reeval/recalc windows
+    # - Option + Shift + W = Focus Screen 1
+    # - Option + Shift + E = Focus Screen 2
+    # - Option + Shift + R = Focus Screen 3
+    # - Control + Option + Shift + Z = Relaunch Amethyst
+    # - Control + Option + Shift + Number = Throw focused window to space matching number
     "amethyst"
   ];
 }
