6bae2632e37bffa0d632618fe719b6e0fd0fbddb
David Asabina <vid@bina.me>
2023-10-04 21:13:52 +0900

    chore: Tangle Emacs nix-darwin conf
    this is a monsterload, painstakingly and hackishly put together during
    the flight Incheon to Frankfurt. Hope I don't come to regret it later on
    as some rough changes have been made here that I can't verify beyond a
    syntax prebuild code-validity check with `darwin-rebuild check`.

--- emacs/nix-darwin.nix
+++ emacs/nix-darwin.nix
@@ -1,21 +1,26 @@
-{ pkgs, lib, inputs, ... }:
-let
-  my-emacs = pkgs.emacs29-macport.pkgs.withPackages (epkgs: (
-    with epkgs; [
-      notmuch
-      vterm
-      pdf-tools
-    ]
-  ));
-in
+# Tangled from README.org
+{ pkgs, ... }:
+
 {
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     my-emacs
+  ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+      my-emacs = pkgs.emacs29-macport.pkgs.withPackages (epkgs: (
+        with epkgs; [
+          notmuch
+          vterm
+          pdf-tools
+        ]
+      ));
+    })
   ];
 
   # See https://itecnote.com/tecnote/why-emacsclient-cant-find-socket-after-executing-emacs-daemon/
   services.emacs = {
     enable = true;
-    package = my-emacs;
+    package = pkgs.my-emacs;
   };
 }
