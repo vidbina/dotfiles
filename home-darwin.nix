f1952438490832e83e243e32df389436e9d65f8f
David Asabina <vid@bina.me>
2023-11-17 20:58:02 +0100

    chore: Isolate hm part of emacs config

--- home-darwin.nix
+++ home-darwin.nix
@@ -4,21 +4,13 @@
 {
   imports = [
     ./vim.nix
+    ./emacs/home.nix
   ];
 
   home.stateVersion = "23.05";
 
   home.packages = with pkgs; [
   ];
-
-  # home.file.".emacs.d".source = config.lib.file.mkOutOfStoreSymlink ./emacs;
-  # TODO: Fix hack of hardcoded dotfiles path
-  # NOTE: This repo must be checked out to ~/Code/vidbina/dotfiles
-  # A hardcoded .emacs.d source is used because mkOutOfStoreSymlink ./emacs
-  # does not seem to work on macOS.
-  # See https://discourse.nixos.org/t/accessing-home-manager-config-in-flakes/19864/8
-  # See https://github.com/nix-community/home-manager/issues/2085#issuecomment-861427318
-  home.file.".emacs.d".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/vidbina/dotfiles/emacs";
 
   # NOTE: Copied from dev.nix
   # TODO: Figure out how to re-use dev.nix config for Darwin and Linux
