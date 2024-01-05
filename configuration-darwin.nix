4d7b4cfc364e69806fd042b272937035d5518bf1
David Asabina <vid@bina.me>
2024-01-05 12:42:17 +0100

    chore: Disable hacky nix-darwin activationScript

--- configuration-darwin.nix
+++ configuration-darwin.nix
@@ -157,39 +157,40 @@
     home = "/Users/vidbina";
   };
 
-  # Use activation scripts to set up Spotlight visibility of nix-darwin apps
-  # See https://github.com/LnL7/nix-darwin/issues/214#issuecomment-1230730292
-  system.activationScripts.applications.text = lib.mkForce ''
-    echo "setting up ~/Applications..." >&2
-    applications="$HOME/Applications"
-    nix_apps="$applications/Nix Apps"
+  # # Use activation scripts to set up Spotlight visibility of nix-darwin apps
+  # # - https://github.com/LnL7/nix-darwin/issues/214#issuecomment-1230730292
+  # # - https://github.com/nix-community/home-manager/issues/1341
+  # system.activationScripts.applications.text = lib.mkForce ''
+  #   echo "setting up ~/Applications..." >&2
+  #   applications="$HOME/Applications"
+  #   nix_apps="$applications/Nix Apps"
 
-    # Needs to be writable by the user so that home-manager can symlink into it
-    if ! test -d "$applications"; then
-        mkdir -p "$applications"
-        chown ${username}: "$applications"
-        chmod u+w "$applications"
-    fi
+  #   # Needs to be writable by the user so that home-manager can symlink into it
+  #   if ! test -d "$applications"; then
+  #       mkdir -p "$applications"
+  #       chown ${username}: "$applications"
+  #       chmod u+w "$applications"
+  #   fi
 
-    # Delete the directory to remove old links
-    rm -rf "$nix_apps"
-    mkdir -p "$nix_apps"
-    find ${config.system.build.applications}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
-            # Spotlight does not recognize symlinks, it will ignore directory we link to the applications folder.
-            # It does understand MacOS aliases though, a unique filesystem feature. Sadly they cannot be created
-            # from bash (as far as I know), so we use the oh-so-great Apple Script instead.
-            /usr/bin/osascript -e "
-                set fileToAlias to POSIX file \"$src\"
-                set applicationsFolder to POSIX file \"$nix_apps\"
-                tell application \"Finder\"
-                    make alias file to fileToAlias at applicationsFolder
-                    # This renames the alias; 'mpv.app alias' -> 'mpv.app'
-                    set name of result to \"$(rev <<< "$src" | cut -d'/' -f1 | rev)\"
-                end tell
-            " 1>/dev/null
-        done
-  '';
+  #   # Delete the directory to remove old links
+  #   rm -rf "$nix_apps"
+  #   mkdir -p "$nix_apps"
+  #   find ${config.system.build.applications}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  #       while read src; do
+  #           # Spotlight does not recognize symlinks, it will ignore directory we link to the applications folder.
+  #           # It does understand MacOS aliases though, a unique filesystem feature. Sadly they cannot be created
+  #           # from bash (as far as I know), so we use the oh-so-great Apple Script instead.
+  #           /usr/bin/osascript -e "
+  #               set fileToAlias to POSIX file \"$src\"
+  #               set applicationsFolder to POSIX file \"$nix_apps\"
+  #               tell application \"Finder\"
+  #                   make alias file to fileToAlias at applicationsFolder
+  #                   # This renames the alias; 'mpv.app alias' -> 'mpv.app'
+  #                   set name of result to \"$(rev <<< "$src" | cut -d'/' -f1 | rev)\"
+  #               end tell
+  #           " 1>/dev/null
+  #       done
+  # '';
 
   homebrew = {
     enable = true;
