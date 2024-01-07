7c828a6a246a34d5e7162bb500f28c93a21824b4
David Asabina <vid@bina.me>
2024-01-07 13:30:16 +0100

    chore: Log emacs launchd service into /tmp/

--- emacs/nix-darwin.nix
+++ emacs/nix-darwin.nix
@@ -1,5 +1,5 @@
 # Tangled from README.org
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
@@ -14,8 +14,14 @@
 
   ];
 
-  services.emacs = {
-    enable = true;
-    package = pkgs.my-emacs;
+  # based on https://github.com/LnL7/nix-darwin/blob/0dd382b70c351f528561f71a0a7df82c9d2be9a4/modules/services/emacs.nix#L45-L50
+  launchd.user.agents.my-emacs = {
+    path = [ config.environment.systemPath ];
+    serviceConfig = {
+      ProgramArguments = [ "${pkgs.my-emacs}/bin/emacs" "--fg-daemon=main" ];
+      RunAtLoad = true;
+      StandardErrorPath = "/tmp/emacs.stderr.log";
+      StandardOutPath = "/tmp/emacs.stdout.log";
+    };
   };
 }
