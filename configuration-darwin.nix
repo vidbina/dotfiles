3d46a2d483496add4054077a2b3b3d1376554b20
David Asabina <vid@bina.me>
2024-09-12 23:09:56 +0900

    chore: Uncomment nix installation to make it explicit

--- configuration-darwin.nix
+++ configuration-darwin.nix
@@ -64,7 +64,7 @@
   # General nix-darwin settings
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  nix.package = pkgs.nix;
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
