44196ba72824bd2eef5d2a653200d4e0d20b5a51
David Asabina <vid@bina.me>
2024-10-18 18:39:27 +0200

    feat: Set <Leader>i to show diagnostics in nvim

--- vim.nix
+++ vim.nix
@@ -45,6 +45,8 @@
     '';
     extraLuaConfig = ''
       require'lspconfig'.gleam.setup{}
+      -- Float diagnostics window through custom mapping
+      vim.keymap.set('n', '<Leader>i', function() vim.diagnostic.open_float(nil, {focus=false, scope='cursor'}) end)
       require'nvim-treesitter.configs'.setup{
         highlight = {
           enable = true,
