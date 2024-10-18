8812518b296a5ecef6c9b638e26f0c2b469bee54
David Asabina <vid@bina.me>
2024-10-18 17:59:45 +0200

    chore: Isolate nvim LSP and tree-sitter config and enable highlight

--- vim.nix
+++ vim.nix
@@ -18,6 +18,8 @@
       vim-dim
       vim-fugitive
       vim-gitgutter
+      nvim-lspconfig
+      nvim-treesitter.withAllGrammars
     ];
     vimdiffAlias = true;
     withRuby = true;
@@ -43,6 +45,16 @@
     '';
     extraLuaConfig = ''
       require'lspconfig'.gleam.setup{}
+      require'nvim-treesitter.configs'.setup{
+        highlight = {
+          enable = true,
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      }
     '';
   };
 }
